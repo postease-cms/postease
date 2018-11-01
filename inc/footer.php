5e33cdb47e089394fd11957237ed00396bb8d407
h-kuwayama <h.kuwayama@khiten.co.jp>
2018-11-01 13:01:32 +0900

    release v2.8.13

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,7 @@
 <p class="footer_rights">2015-<?=date('Y')?> POSTEASE.ORG ALL RIGHTS RESERVED.</p>
-<script src="js/scripts.js?v=<?=$_SESSION[$session_key]['configs']['postease_version']?>"></script>
+<script src="plugin/bootstrap/js/bootstrap.min.js"></script>
+<script src="plugin/fancybox2/jquery.fancybox.pack.js"></script>
+<script src="js/common.js?v=<?=$_SESSION[$session_key]['configs']['postease_version']?>"></script>
 <?php $js_exclusive = './js/_'. $view_page . '.js'?>
 <?php if (file_exists($js_exclusive)):?>
 <script src="<?=$js_exclusive?>"></script>
